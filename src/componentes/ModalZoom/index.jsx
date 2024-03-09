b0c53d7f08d26f4c3dad9a84be9576d32397a040
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-09 08:34:30 -0300

    apagando fotos desnecessarias

---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,13 @@
+
+export const ModalZoom = () =>{
+    return(
+        <>
+            <dialog>
+                <p>Meu paragrafo</p>
+                <form action="">    
+                    <button>Ok my Friend</button>
+                </form>
+            </dialog>
+        </>
+    )
+}
